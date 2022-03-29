34b462dc91889814bade34957c5400599b76e015
Syed Aman <syednashikaman@gmail.com>
2022-03-28 21:49:20 -0400

    Write the file exactly like in figure 1-1 as instructed.

--- Figures/1-1/main.cpp
+++ Figures/1-1/main.cpp
@@ -1,5 +1,12 @@
-#include <iostream>
+/*
+ * File: HelloWorld.cpp
+ * --------------------
+ *  This is file is adapted from the example
+ *  on page 1 of Kernighan and Ritchie's
+ *  book The C Programming Language.
+ */
 
+#include <iostream>
 using namespace std;
 
 int main()
